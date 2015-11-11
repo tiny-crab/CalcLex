795ae2336807f2755de1b883d28173976e8b9126
zakattak <zachary.jay.crabtree@gmail.com>
2015-11-11 15:33:50 -0800

    not using xcode now...

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "calcLex.h"
+
+int main(int argc, const char *argv[])
+{
+    system("pwd");
+    //get the file name from the command line input
+    if(argc > 1 && (!calcLexOpen( argv[1]) ) )
+    {
+        cout << "Sorry, I can't find/open that file!" << argv[1] << endl;
+        exit(1);
+    }
+    
+    int token;
+    while( (token = calcLex()) != endOfFileSym )
+    {
+        cout << "Token(" << token << "): " << endl;
+    }
+    
+    return 0;
+}
