c327db128e1d0e76ffe59e64891dc8d2ecfb1937
zakattak <zachary.jay.crabtree@gmail.com>
2015-11-11 12:23:56 -0800

    first commit

---
+++ CalcLex/CalcLex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CalcLex
+//
+//  Created by Zak Crabtree on 11/11/15.
+//  Copyright © 2015 Zach Crabtree. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
