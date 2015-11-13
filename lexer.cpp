9e6a9b19e5e84378c0303bf1e16bc0323dd502ee
zakattak <zachary.jay.crabtree@gmail.com>
2015-11-13 11:43:34 -0800

    decimal stuff

--- lexer.cpp
+++ lexer.cpp
@@ -8,17 +8,18 @@ ifstream filestream;
 bool calcLexOpen(const char filename[])
 {
     filestream.open(filename, ios_base::in);
+    //returning true or false if the filestream is open
     return filestream.is_open();
 }
 
-//this function resets the position of the scanner in the file
+//this function resets the token's info
 void calcTextClear()
 {
     calcTextPos = 0;
     calcText[calcTextPos] = 0;
 }
 
-//this function
+//this function pushes chars onto the token's info cstring
 void calcTextAppend(int currentChar)
 {
     if(calcTextPos >= (CALCTEXT_MAX - 1) )
@@ -29,14 +30,17 @@ void calcTextAppend(int currentChar)
     calcText[calcTextPos] = 0;
 }
 
+//this function finds the next token and outputs it
 int calcLex()
 {
     char currentChar;
 
     while(1) //infinite loop, breaks through a return statement
     {
+        //clear out the token info from last time
         calcTextClear();
-        currentChar = filestream.get(); //get the next char!
+        //get the next char!
+        currentChar = filestream.get();
 
         //used to ignore whitespace as a token
         while( currentChar == ' ' || currentChar == '\t' || currentChar == '\n')
@@ -44,33 +48,46 @@ int calcLex()
           currentChar = filestream.get();
         };
 
+        //if the current char is the end of a file, it returns the end of file symbol
         if(currentChar == EOF)
         {
             return endOfFileSym;
         }
 
+        //this is the comment logic: if the next chars are /*...
         if(followingChars("/*", currentChar))
         {
+            //then while it isn't ending the line, or finding the end of comment sentinel...
             while(currentChar != '\n' && !followingChars("*/", currentChar))
             {
+              //move onto the next character
               currentChar = filestream.get();
             }
+            //move to the next character after the comment or line has ended
             currentChar = filestream.get();
-            calcTextClear();
+            //hop back up to the top of the while loop
+            continue;
         }
+        //if the chars ":=" are found, return the assignment symbol
         if(followingChars(":=", currentChar))
         {
             return assignSym;
         }
+        //if the chars "read" are found, return the read symbol
         if(followingChars("read", currentChar))
         {
             return readSym;
         }
+        //if the chars "write" are found, return the write symbol
         if(followingChars("write", currentChar))
         {
             return writeSym;
         }
+
+        //this line must go below the followingChars checking, because otherwise you get double chars in your token info
         calcTextAppend(currentChar);
+
+        //checks for identifier strings, must only use letters
         if( (currentChar >= 'A' && currentChar <= 'Z') || (currentChar >= 'a' && currentChar <= 'z'))
         {
             //messy line, but gets new char and checks between capital letters and lowercase letters on the ascii table
@@ -79,14 +96,67 @@ int calcLex()
                 calcTextAppend(currentChar);
             }
 
+            //once the currentChar falls off the end of the identifier string, it must hop back to read the next one
             filestream.unget();
+            //returns the identifier int
             return identifier;
         }
-        if(currentChar >= '0' && currentChar <= '9')
+
+        //numeric constant must start with a digit, never with a decimal
+        //if the current char is a number, or float starting with a decimal...
+        if((currentChar >= '1' && currentChar <= '9') || currentChar == '.')
         {
-            while( (currentChar = filestream.get()) >= '0' && currentChar <= '9')
+            cout << "I found a numeric constant" << endl;
+            //this makes sure it does not have more than one decimal, and it starts with a decimal
+            if(currentChar == '.')
             {
-                calcTextAppend(currentChar);
+              cout << "It had a decimal at the front." << endl;
+              while( (currentChar = filestream.get()) >= '0' && currentChar <= '9')
+              {
+                  cout << "It had these numbers following the decimal:" << currentChar;
+                  calcTextAppend(currentChar);
+              }
+            }
+            //this makes sure that it can have any number of digits before a decimal, but only one decimal
+            else
+            {
+              cout << "It didn't start with a decimal." << endl;
+              //this can see an unlimited number of digits before a decimal...
+              while( ((currentChar = filestream.get()) >= '0' && currentChar <= '9') || currentChar == '.')
+              {
+                  cout << "It had these numbers before the decimal: " << currentChar;
+                  calcTextAppend(currentChar);
+                  //but once it finds a decimal...
+                  if(currentChar == '.')
+                  {
+                    cout << "I found a decimal here";
+                    //get the next char
+                    currentChar = filestream.get();
+                    //if it isn't a digit, throw an exception (10 works because of the way main.cpp handles proper tokens)
+                    if(!(currentChar >= '0' && currentChar <= '9'))
+                    {
+                      cout << "I got a number after the decimal";
+                      return currentChar;
+                    }
+                    //infinite loop to check for remaining numbers
+                    while(1)
+                    {
+                      currentChar = filestream.get();
+                      if(currentChar >= '0' && currentChar <= '9')
+                      {
+                        calcTextAppend(currentChar);
+                      }
+                      if(currentChar == ' ' || currentChar == '\t' || currentChar == '\n')
+                      {
+                        return numConst;
+                      }
+                      else
+                      {
+                        return currentChar;
+                      }
+                    }
+                  }
+              }
             }
 
             filestream.unget();
